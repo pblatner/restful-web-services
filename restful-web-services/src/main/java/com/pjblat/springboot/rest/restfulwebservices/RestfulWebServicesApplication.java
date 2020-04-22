c0449bb56d266e24b606d145cd474f9c043f8a5f
Pete Blatner <pjblat@us.ibm.com>
2020-04-22 13:44:05 -0500

    Updating the actuate and HAL properties.

--- restful-web-services/src/main/java/com/pjblat/springboot/rest/restfulwebservices/RestfulWebServicesApplication.java
+++ restful-web-services/src/main/java/com/pjblat/springboot/rest/restfulwebservices/RestfulWebServicesApplication.java
@@ -3,6 +3,11 @@ package com.pjblat.springboot.rest.restfulwebservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * Link to the HAL Browser for the application - http://localhost:8080
+ *    with al actuator endpoints: http://localhost:8080/browser/index.html#/actuator
+ * Link to the Spring actuator data - http://localhost:8080/actuator
+ */
 @SpringBootApplication
 public class RestfulWebServicesApplication {
 
